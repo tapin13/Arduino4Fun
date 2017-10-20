f78b9281a9bb843fe9662e37627b9675210c441c
tapin13 <tapin13@gmail.com>
2017-10-20 21:57:52 +0300

    nano test

---
+++ nano_led_external_blink/nano_led_external_blink.ino
@@ -0,0 +1,12 @@
+#define BOARD_LED 5
+
+void setup() {
+  pinMode(BOARD_LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(BOARD_LED, HIGH);
+  delay(1000);
+  digitalWrite(BOARD_LED, LOW);
+  delay(500);
+}
